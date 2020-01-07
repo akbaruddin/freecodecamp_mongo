03671ff44b51f518ec9b36ee748ec6c81462a64e
akbar <akbaruddin@live.in>
2020-01-08 00:01:46 +0530

    date

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ fastify.get('/', async (request, reply) => {
 })
 
 fastify.get('/api/timestamp/:date_string', async (request, reply) => {
-  return { hello: 'world' }
+  const DATE_STRING = request.params.date_string;
+
+  if (DATE_STRING === "") {
+    const date = new Date(Date.now());
+    return {"unix": date.getTime() ,"utc": date.toUTCString()};
+  } else if (DATE_STRING.includes("-")) {
+    const date = new Date(DATE_STRING);
+    return {"unix": date.getTime() ,"utc": date.toUTCString()};
+  } else {
+    const unixtime = new Date(DATE_STRING * 1000);
+    console.log(unixtime.toString())
+    if (unixtime.toString() !== "Invalid Date") {
+      return {"unix": unixtime.getTime() ,"utc": unixtime.toUTCString()};
+    } else {
+      return {"error" : "Invalid Date" };
+    }
+  }
 })
 
 // Run the server!
